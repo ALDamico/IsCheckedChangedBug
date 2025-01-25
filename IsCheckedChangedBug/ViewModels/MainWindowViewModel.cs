94e8fc2f9a93cf3776adefb29c1e9c1129b751ba
Andrea Luciano Damico <luciano.damico@mbsengineering.it>
2025-01-25 16:30:28 +0100

    Example

---
+++ IsCheckedChangedBug/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace IsCheckedChangedBug.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public MainWindowViewModel()
+    {
+        OnIsCheckedCmd = new AsyncRelayCommand<bool>(Execute);
+        IsChecked = false;
+        Text = "";
+    }
+
+    private Task Execute(bool arg)
+    {
+        Task.Delay(100);
+        Text += Environment.NewLine + $"Arg is {arg}";
+        return Task.Delay(100);
+    }
+
+    [ObservableProperty] private bool _isChecked;
+    [ObservableProperty] private string _text;
+    public ICommand OnIsCheckedCmd { get; }
+}
